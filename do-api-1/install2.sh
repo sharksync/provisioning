98d5d4a0e1c590b133e0c0f9b159143395817317
Adrian Herridge <ah@Adrians-MacBook-Pro.local>
2016-08-15 14:03:35 +0100

    added api server scripts

---
+++ do-api-1/install2.sh
@@ -0,0 +1,12 @@
+yum -y install npm 
+nvm install 5
+npm install hapi
+npm install good
+npm install calibrate
+npm install joi
+npm install async
+npm install boom
+npm install moment
+npm install cassandra-driver
+npm install good-squeeze
+npm install good-console
